a6150c99dfc368d503555430e929ca5b0c8fc8f3
Dominique Martinet <dominique.martinet@atmark-techno.com>
2025-01-09 17:57:40 +0100

    zram: check comp is non-NULL before calling comp_destroy This is a pre-requisite for the backport of commit 74363ec674cb ("zram: fix uninitialized ZRAM not releasing backing device"), which has been implemented differently in commit 7ac07a26dea7 ("zram: preparation for multi-zcomp support") upstream.
    We only need to ensure that zcomp_destroy is not called with a NULL
    comp, so add this check as the other commit cannot be backported easily.
    
    Stable-dep-of: 74363ec674cb ("zram: fix uninitialized ZRAM not releasing backing device")
    Link: https://lore.kernel.org/Z3ytcILx4S1v_ueJ@codewreck.org
    Suggested-by: Kairui Song <kasong@tencent.com>
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- google-modules/soc/gs/drivers/block/zram/zram_drv.c
+++ google-modules/soc/gs/drivers/block/zram/zram_drv.c
@@ -1538,7 +1538,8 @@ static void zram_reset_device(struct zram *zram)
 	zram_meta_free(zram, zram->disksize);
 	zram->disksize = 0;
 	init_zram_stat(zram);
-	zcomp_destroy(zram->comp);
+	if (zram->comp)
+		zcomp_destroy(zram->comp);
 	zram->comp = NULL;
 	reset_bdev(zram);
 
