8a5cc222b5cd80a6cad04322799cacb52efe1964
Dark-Matter7232 <me@const.eu.org>
2025-08-16 21:24:13 +0200

    kernel/power/swap: Use copy_page for full page copy
    Some architectures have implemented optimized copy_page for full page
    copying, such as arm.
    
    On my arm platform, use the copy_page helper for single page copying is
    about 10 percent faster than memcpy.
    
    Change-Id: Ie28de9ef5954d0c232b418f382471bc7c125563f
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- kernel/power/swap.c
+++ kernel/power/swap.c
@@ -876,7 +876,7 @@ static int save_compressed_image(struct swap_map_handle *handle,
 			for (off = 0;
 			     off < CMP_HEADER + data[thr].cmp_len;
 			     off += PAGE_SIZE) {
-				memcpy(page, data[thr].cmp + off, PAGE_SIZE);
+				copy_page(page, data[thr].cmp + off);
 
 				trace_android_vh_encrypt_page(page);
 				ret = swap_write_page(handle, page, &hb);
